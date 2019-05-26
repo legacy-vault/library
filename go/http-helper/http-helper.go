7a31140a3f2efae0f886b644f1fe95d18f1a3de7
Legacy-Vault <legacy-vault@yandex.com>
2019-05-26 21:30:42 +0300

    HTTP Helper

---
+++ go/http-helper/http-helper.go
@@ -0,0 +1,11 @@
+package httphelper
+
+// HTTP Helper.
+
+// This Package contains various useful Functions which help in processing HTTP
+// Requests.
+
+// Errors.
+const (
+	ErrNotPointer = "Not a Pointer"
+)
